e5413301e9d581e105705b3cd438e51dc7f91e8d
anas20023 <yorder07@gmail.com>
2024-01-06 13:23:30 +0600

    Add flag change functionality

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
+/// Variable and Objects Access
+
+let from_img = document.querySelector("#from_img");
+let to_img = document.querySelector("#to_img");
 const Optn = document.querySelectorAll(".select_itm");
+
+/// Currency List
 for (let select of Optn) {
   for (let crr_code in countryList) {
     let NewOption = document.createElement("option");
@@ -12,3 +18,17 @@ for (let select of Optn) {
     select.appendChild(NewOption);
   }
 }
+/// For Flag Change
+Optn.forEach((sbt) => {
+  sbt.addEventListener("change", (evt) => {
+    let New_src = `https://flagsapi.com/${
+      countryList[evt.target.value]
+    }/flat/64.png`;
+    if (evt.target.name === "from") {
+      from_img.src = New_src;
+    } else if (evt.target.name === "to") {
+      to_img.src = New_src;
+    }
+  });
+});
+/// For Currency Convert
