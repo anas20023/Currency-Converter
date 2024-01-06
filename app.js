a27baf116ca2bdf2a154871764afc6fbd8cb9f67
anas20023 <yorder07@gmail.com>
2024-01-06 14:59:32 +0600

    Add currency conversion functionality

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ let to_img = document.querySelector("#to_img");
 const Optn = document.querySelectorAll(".select_itm");
 let Base_url =
   "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/bdt.json";
+let Submt_btn = document.querySelector("#submit");
+let Amnt = document.querySelector("#Amount");
+let Amnt_val, newURL;
 
 /// Currency List
 for (let select of Optn) {
@@ -34,3 +37,20 @@ Optn.forEach((sbt) => {
   });
 });
 /// For Currency Convert
+Submt_btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  Amnt_val = parseFloat(Amnt.value);
+  // newURL=`${Base_url}/${}`
+  if (Amnt_val < 1) {
+    Amnt.value = 1;
+    Amnt_val = parseFloat(Amnt.value);
+  }
+  Get_rate();
+});
+
+/// Data From API
+
+let Get_rate = async () => {
+  //let response = await fetch();
+  console.log(Amnt_val);
+};
