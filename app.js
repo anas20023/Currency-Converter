29d773fee4eacde6e12b9dc49922ffd106c2579f
anas20023 <yorder07@gmail.com>
2024-01-06 09:55:23 +0600

    Add country codes to select options

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-for(flags in Count)
-{
-    
+const Optn = document.querySelectorAll(".select_itm");
+for (let select of Optn) {
+  for (let crr_code in countryToCurrency) {
+    let NewOption = document.createElement("option");
+    NewOption.innerText = crr_code;
+    NewOption.value = crr_code;
+    if (select.name === "from" && crr_code === "USD") {
+      NewOption.selected = "selected";
+    }
+    select.appendChild(NewOption);
+  }
 }
