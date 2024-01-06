7b5a8833f280dc4c64ea2b334da05c777642a874
anas20023 <yorder07@gmail.com>
2024-01-06 10:09:29 +0600

    Update currency selection options

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const Optn = document.querySelectorAll(".select_itm");
 for (let select of Optn) {
-  for (let crr_code in countryToCurrency) {
+  for (let crr_code in countryList) {
     let NewOption = document.createElement("option");
     NewOption.innerText = crr_code;
     NewOption.value = crr_code;
     if (select.name === "from" && crr_code === "USD") {
+      NewOption.selected = "selected";
+    } else if (select.name === "to" && crr_code === "BDT") {
       NewOption.selected = "selected";
     }
     select.appendChild(NewOption);
